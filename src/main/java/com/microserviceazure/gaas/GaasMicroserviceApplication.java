efd8dd15692fd2f87a9573089e7e52b27f917e49
Isha Mehta <isha.mehta@publicissapient.com>
2023-01-23 02:19:55 +0000

    first commit

---
+++ src/main/java/com/microserviceazure/gaas/GaasMicroserviceApplication.java
@@ -0,0 +1,20 @@
+package com.microserviceazure.gaas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GaasMicroserviceApplication {
+
+	@GetMapping("message")
+	public String getMessage(){
+		return "Hi";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(GaasMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microserviceazure/gaas/GaasMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microserviceazure.gaas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaasMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
